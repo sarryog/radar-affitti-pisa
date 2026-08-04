befc08e10a4e4c41a9ee902d3b00208a86e65185
sarryog <zn427py2pm@privaterelay.appleid.com>
2026-08-04 23:34:52 +0200

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,1 +1,17 @@
+import os
+import requests
 
+
+def send_message(text):
+
+    token = os.environ["TELEGRAM_TOKEN"]
+    chat_id = os.environ["TELEGRAM_CHAT_ID"]
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+
+    data = {
+        "chat_id": chat_id,
+        "text": text
+    }
+
+    requests.post(url, data=data)
